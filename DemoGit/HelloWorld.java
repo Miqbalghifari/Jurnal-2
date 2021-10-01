decece38ac4b1846d106a8642d80ac798d7de364
miqbalghifari <iqbalgifarie@gmail.com>
2021-10-01 08:26:19 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String Datadiri = "Nama : M.iqbal al ghifari Nim: 670210013 IPK: 4.0";
+	System.out.println(Datadiri);
+	}
+}
